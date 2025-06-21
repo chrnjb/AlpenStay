eeb22a73e32aab0009a291dec2c9028cf980a755
Chiranjib Muduli <94298710+chrnjb@users.noreply.github.com>
2025-06-21 09:45:49 +0530

    Create data.js

---
+++ init/data.js
@@ -0,0 +1,352 @@
+const sampleListings = [
+  {
+    title: "Cozy Beachfront Cottage",
+    description:
+      "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/shangai-cityscape-4k-9q.jpg",
+    },
+    price: 1500,
+    location: "Malibu",
+    country: "United States",
+  },
+  {
+    title: "Modern Loft in Downtown",
+    description:
+      "Stay in the heart of the city in this stylish loft apartment. Perfect for urban explorers!",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/madeira-ocean-water-sunrise-rocks-down-long-exposure-k1.jpg",
+    },
+    price: 1200,
+    location: "New York City",
+    country: "United States",
+  },
+  {
+    title: "Mountain Retreat",
+    description:
+      "Unplug and unwind in this peaceful mountain cabin. Surrounded by nature, it's a perfect place to recharge.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/forests-trees-swamp-rays-of-light-ob.jpg",
+    },
+    price: 1000,
+    location: "Aspen",
+    country: "United States",
+  },
+  {
+    title: "Historic Villa in Tuscany",
+    description:
+      "Experience the charm of Tuscany in this beautifully restored villa. Explore the rolling hills and vineyards.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/autumn-roads-4k-os.jpg",
+    },
+    price: 2500,
+    location: "Florence",
+    country: "Italy",
+  },
+  {
+    title: "Secluded Treehouse Getaway",
+    description:
+      "Live among the treetops in this unique treehouse retreat. A true nature lover's paradise.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/air-balloons-flying-over-cappadocia-5k-u1.jpg",
+    },
+    price: 800,
+    location: "Portland",
+    country: "United States",
+  },
+  {
+    title: "Beachfront Paradise",
+    description:
+      "Step out of your door onto the sandy beach. This beachfront condo offers the ultimate relaxation.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/windmill-village-in-netherlands-xt.jpg",
+    },
+    price: 2000,
+    location: "Cancun",
+    country: "Mexico",
+  },
+  {
+    title: "Rustic Cabin by the Lake",
+    description:
+      "Spend your days fishing and kayaking on the serene lake. This cozy cabin is perfect for outdoor enthusiasts.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/a-forest-filled-with-lots-of-trees-covered-in-fog-jg.jpg",
+    },
+    price: 900,
+    location: "Lake Tahoe",
+    country: "United States",
+  },
+  {
+    title: "Luxury Penthouse with City Views",
+    description:
+      "Indulge in luxury living with panoramic city views from this stunning penthouse apartment.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/big-rock-surrounded-by-the-body-of-water-bl.jpg",
+    },
+    price: 3500,
+    location: "Los Angeles",
+    country: "United States",
+  },
+  {
+    title: "Ski-In/Ski-Out Chalet",
+    description:
+      "Hit the slopes right from your doorstep in this ski-in/ski-out chalet in the Swiss Alps.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/phang-nga-bay-ls.jpg",
+    },
+    price: 3000,
+    location: "Verbier",
+    country: "Switzerland",
+  },
+  {
+    title: "Safari Lodge in the Serengeti",
+    description:
+      "Experience the thrill of the wild in a comfortable safari lodge. Witness the Great Migration up close.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/mountain-range-cansiglio-italy-zk.jpg",
+    },
+    price: 4000,
+    location: "Serengeti National Park",
+    country: "Tanzania",
+  },
+  {
+    title: "Historic Canal House",
+    description:
+      "Stay in a piece of history in this beautifully preserved canal house in Amsterdam's iconic district.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/dune-valley-8k-8d.jpg",
+    },
+    price: 1800,
+    location: "Amsterdam",
+    country: "Netherlands",
+  },
+  {
+    title: "Private Island Retreat",
+    description:
+      "Have an entire island to yourself for a truly exclusive and unforgettable vacation experience.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/lake-storm-morning-8k-mr.jpg",
+    },
+    price: 10000,
+    location: "Fiji",
+    country: "Fiji",
+  },
+  {
+    title: "Charming Cottage in the Cotswolds",
+    description:
+      "Escape to the picturesque Cotswolds in this quaint and charming cottage with a thatched roof.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/pier-side-blue-evening-ld.jpg",
+    },
+    price: 1200,
+    location: "Cotswolds",
+    country: "United Kingdom",
+  },
+  {
+    title: "Historic Brownstone in Boston",
+    description:
+      "Step back in time in this elegant historic brownstone located in the heart of Boston.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/central-florida-hammock-by-the-lake-i7.jpg",
+    },
+    price: 2200,
+    location: "Boston",
+    country: "United States",
+  },
+  {
+    title: "Beachfront Bungalow in Bali",
+    description:
+      "Relax on the sandy shores of Bali in this beautiful beachfront bungalow with a private pool.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/mountains-evening-5k-ib.jpg",
+    },
+    price: 1800,
+    location: "Bali",
+    country: "Indonesia",
+  },
+  {
+    title: "Mountain View Cabin in Banff",
+    description:
+      "Enjoy breathtaking mountain views from this cozy cabin in the Canadian Rockies.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/arches-national-park-at-night-5k-aq.jpg",
+    },
+    price: 1500,
+    location: "Banff",
+    country: "Canada",
+  },
+  {
+    title: "Art Deco Apartment in Miami",
+    description:
+      "Step into the glamour of the 1920s in this stylish Art Deco apartment in South Beach.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/boat-and-duck-in-the-calm-of-dawn-us.jpg",
+    },
+    price: 1600,
+    location: "Miami",
+    country: "United States",
+  },
+  {
+    title: "Tropical Villa in Phuket",
+    description:
+      "Escape to a tropical paradise in this luxurious villa with a private infinity pool in Phuket.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/whangaroa-blues-northland-new-zealand-rx.jpg",
+    },
+    price: 3000,
+    location: "Phuket",
+    country: "Thailand",
+  },
+  {
+    title: "Historic Castle in Scotland",
+    description:
+      "Live like royalty in this historic castle in the Scottish Highlands. Explore the rugged beauty of the area.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/yello-lights-brown-wall-building-5k-ck.jpg",
+    },
+    price: 4000,
+    location: "Scottish Highlands",
+    country: "United Kingdom",
+  },
+  {
+    title: "Desert Oasis in Dubai",
+    description:
+      "Experience luxury in the middle of the desert in this opulent oasis in Dubai with a private pool.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/golden-gate-bridge-evening-5k-8o.jpg",
+    },
+    price: 5000,
+    location: "Dubai",
+    country: "United Arab Emirates",
+  },
+  {
+    title: "Rustic Log Cabin in Montana",
+    description:
+      "Unplug and unwind in this cozy log cabin surrounded by the natural beauty of Montana.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/dubai-palm-trees-sunset-reflection-5a.jpg",
+    },
+    price: 1100,
+    location: "Montana",
+    country: "United States",
+  },
+  {
+    title: "Beachfront Villa in Greece",
+    description:
+      "Enjoy the crystal-clear waters of the Mediterranean in this beautiful beachfront villa on a Greek island.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/mount-fuji-across-reflective-waters-be.jpg",
+    },
+    price: 2500,
+    location: "Mykonos",
+    country: "Greece",
+  },
+  {
+    title: "Eco-Friendly Treehouse Retreat",
+    description:
+      "Stay in an eco-friendly treehouse nestled in the forest. It's the perfect escape for nature lovers.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/snow-beach-g5.jpg",
+    },
+    price: 750,
+    location: "Costa Rica",
+    country: "Costa Rica",
+  },
+  {
+    title: "Historic Cottage in Charleston",
+    description:
+      "Experience the charm of historic Charleston in this beautifully restored cottage with a private garden.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/marine-body-of-water-sky-outdoor-beach-sand-cloud-q8.jpg",
+    },
+    price: 1600,
+    location: "Charleston",
+    country: "United States",
+  },
+  {
+    title: "Modern Apartment in Tokyo",
+    description:
+      "Explore the vibrant city of Tokyo from this modern and centrally located apartment.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/dreamy-beach-sky-island-ocean-evening-b0.jpg",
+    },
+    price: 2000,
+    location: "Tokyo",
+    country: "Japan",
+  },
+  {
+    title: "Lakefront Cabin in New Hampshire",
+    description:
+      "Spend your days by the lake in this cozy cabin in the scenic White Mountains of New Hampshire.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/aurora-across-the-bay-lofoten-5k-q0.jpg",
+    },
+    price: 1200,
+    location: "New Hampshire",
+    country: "United States",
+  },
+  {
+    title: "Luxury Villa in the Maldives",
+    description:
+      "Indulge in luxury in this overwater villa in the Maldives with stunning views of the Indian Ocean.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/sunset-along-the-oregon-coast-at-whaleshead-beach-fj.jpg",
+    },
+    price: 6000,
+    location: "Maldives",
+    country: "Maldives",
+  },
+  {
+    title: "Ski Chalet in Aspen",
+    description:
+      "Hit the slopes in style with this luxurious ski chalet in the world-famous Aspen ski resort.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/beautiful-valley-eng-alm-austria-hs.jpg",
+    },
+    price: 4000,
+    location: "Aspen",
+    country: "United States",
+  },
+  {
+    title: "Secluded Beach House in Costa Rica",
+    description:
+      "Escape to a secluded beach house on the Pacific coast of Costa Rica. Surf, relax, and unwind.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.hdqwalls.com/wallpapers/calm-lake-scenery-uv.jpg",
+    },
+    price: 1800,
+    location: "Costa Rica",
+    country: "Costa Rica",
+  },
+];
+
+module.exports = { data: sampleListings };
