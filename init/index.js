c5847ca5fdfadbae826df83cd4548c85cf20823c
Chiranjib Muduli <94298710+chrnjb@users.noreply.github.com>
2025-06-21 09:46:54 +0530

    index.js

--- init/index.js
+++ init/index.js
@@ -1,32 +1,25 @@
-<% layout("/layouts/boilerplate") %>
-  <body>
-    <h3>All Listings</h3>
-    <!-- <form method="GET" action="/listings/new">
-      <button>Create New Listing</button>
-    </form> -->
-  
-<div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 ">
-  <% for(let listing of allListings) { %>
-    <a href="/listings/<%= listing._id %>" class="listing-link">
-  <div class="card">
-    <img 
-      src="<%= listing.image.url %>" 
-      class="card-img-top" 
-      alt="listing_image" 
-      style="height: 20rem" 
-      onerror="this.onerror=null; this.src='https://images.hdqwalls.com/wallpapers/seascape-coast-minimal-qu.jpg';"
-    />
-    <div class="card-img-overlay"></div>
-    <div class="card-body">
-      <p class="card-text">
-        <b><%= listing.title %></b><br>
-        &#8377;<%= listing.price.toLocaleString("en-IN") %> / night
-      </p>
-    </div>
-  </div>
-</a>  
-  <% } %> 
-</div>
-   
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
- 
+const MONGO_URL = "mongodb://127.0.0.1:27017/AlpenStay";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
